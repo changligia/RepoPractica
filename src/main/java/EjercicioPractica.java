48578498afa887522318f27052162fa39cc85796
Ligia Chang <EXTLICR@mapfre.com.pe>
2022-06-11 18:42:44 -0500

    corrigiendo linea 5

--- src/main/java/EjercicioPractica.java
+++ src/main/java/EjercicioPractica.java
@@ -2,7 +2,7 @@ public class EjercicioPractica {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
         System.out.println("Hola Mundo");
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Pirata");
         System.out.println("Hola Mundo");
     }
 }
